07d9f8c2b05bed3d101293af803996a88f666bb7
Daniel Breton <d.breton.suarez@gmail.com>
2020-11-13 23:40:04 +0100

    Adapts to github pages, improves generation script

--- generate.sh
+++ generate.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-pandoc --standalone --from markdown --to html -c style.css -o DBS_CV_remote.html DBS_CV_remote.md && wkhtmltopdf -L 20mm -R 20mm DBS_CV_remote.html DBS_CV_remote.pdf && firefox DBS_CV_remote.pdf && firefox DBS_CV_remote.html
+# TODO: input file.md as an argument
+
+input="DBS_CV_remote.md"
+pdf="DBS_CV_remote.pdf"
+html="index.html"
+css="style.css"
+# index.html is necesary to work with github pages
+
+pandoc --standalone --from markdown --to html -c $css -o $html $input && wkhtmltopdf -L 20mm -R 20mm $html $pdf && firefox $pdf && firefox $html
