4f3a4e3dd8ae5ab297e22f403ca6e9127438d9d8
dbreton <d.breton.suarez@gmail.com>
2023-03-11 11:17:25 +0100

    Improves readability on prerequisites.sh

--- prerequisites.sh
+++ prerequisites.sh
@@ -1,4 +1,39 @@
-br=$(xdg-settings get default-web-browser | cut -d "." -f1)
-echo $br
-sed -i "s/firefox/$br/g" generate.sh
-sudo apt-get install pandoc wkhtmltopdf
+#!/bin/bash
+
+usage() {
+    cat <<EOM
+    Check and install pre-requisites for building the CV from markdown
+	Install the following packages: $packages
+	Usage: $0
+EOM
+    exit 0
+}
+
+browser() {
+	br=$(xdg-settings get default-web-browser | cut -d "." -f1)
+	if [[ -z $br ]]; then
+		echo "Must install some browser"
+		exit 1
+	else
+		echo "Your default browser is $br"
+	fi
+}
+
+requisites() {
+	sudo apt-get install -y $packages
+}
+
+# Main program
+
+packages="pandoc wkhtmltopdf"
+
+while getopts "h" flag; do
+    case "${flag}" in
+        h)
+			usage
+			;;
+    esac
+done
+
+browser
+requisites
