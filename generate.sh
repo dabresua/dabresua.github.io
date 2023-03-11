542c74a44e50116b06382b761ad9df88f006d5bd
dbreton <d.breton.suarez@gmail.com>
2023-03-11 11:18:06 +0100

    Improves readability on generate.sh

--- generate.sh
+++ generate.sh
@@ -2,11 +2,48 @@
 
 # TODO: input file.md as an argument
 
-input="DBS_CV_remote.md"
-pdf="DBS_CV_remote.pdf"
-html="index.html"
-css="responsive.css"
-# index.html is necesary to work with github pages
+usage() {
+    cat <<EOM
+    Builds the CV from markdown to PDF and static HTML
+	Requires some tools to be installed, see prerequisites.sh
+	Uses environment variables instead of arguments
+		MD_FILE: Source markdown file. Default: DBS_CV_remote.md
+		PDF_FILE: Output PDF file. Default DBS_CV_remote.pdf
+		HTML_FILE: Output HTML file. Default index.html
+		CSS_FILE: Style file. Default responsive.css 
+	Usage: $0
+EOM
+    exit 0
+}
 
-pandoc --standalone --from markdown --to html -c $css -o $html $input && \
-wkhtmltopdf -L 20mm -R 20mm $html $pdf && brave-browser $pdf && brave-browser $html
+init() {
+	[[ -z "$MD_FILE" ]] && MD_FILE="DBS_CV_remote.md"
+	[[ -z "$PDF_FILE" ]] && PDF_FILE="DBS_CV_remote.pdf"
+	[[ -z "$HTML_FILE" ]] && HTML_FILE="index.html"
+	[[ -z "$CSS_FILE" ]] && CSS_FILE="responsive.css"
+	browser=$(xdg-settings get default-web-browser | cut -d "." -f1)
+}
+
+generate() {
+	pandoc --standalone --from markdown --to html -c $CSS_FILE -o $HTML_FILE $MD_FILE
+	wkhtmltopdf -L 20mm -R 20mm $HTML_FILE $PDF_FILE	
+}
+
+review() {
+	$browser $PDF_FILE
+	$browser $HTML_FILE
+}
+
+# Main program
+
+while getopts "h" flag; do
+    case "${flag}" in
+        h)
+			usage
+			;;
+    esac
+done
+
+init
+generate
+review
